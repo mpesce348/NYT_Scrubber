105055bda283285f1a40d28bce43edab3d2c0ffe
Michael Pesce <mpesce348@yahoo.com>
2017-08-16 16:09:36 -0400

    routes

--- config/routes.js
+++ config/routes.js
@@ -1,50 +1,10 @@
-// Dependencies
+var React = require("react");
+var router = require("react-router");
 
-var express = require('express');
-var mongoose = require('mongoose');
-var react = require('react');
-var bodyParser = require('body-parser');
+var Route = router.Route;
 
+var Router = router.Router;
 
-// Set Up Express instance
-var app = express();
+var hashHistory = router.hashHistory;
 
-// Set up port to run our express app
-var PORT = process.env.PORT||8080;
-//Set up a router to run express
-var router = express.Router();
-
-// Require routes file to pass our router object
-// require('./config/routes')(router);
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-// Static Files in Our Public Director
-app.use(express.static('public'));
-
-// Have requests go through Router middleware
-app.use(router);
-
-
-
-var db = 'mongodb://localhost/NYT_Scrubber';
-
-// Connect Mongoose to Database
-
-mongoose.connect(db, (error)=>{
-  if (error){
-    console.log(error);
-  } else{
-    console.log('connected to Mongoose');
-  }
-
-});
-
-// Listen to the port
-app.listen(PORT, ()=>{
-  console.log('Listening on Port '+ PORT);
-});
+var IndexRoute = router.IndexRoute;
